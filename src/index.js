741bc53e06423340eec6d8bdead60afaf1c091cb
Tolugaa <guitoluga@gmail.com>
2025-01-22 11:58:18 -0300

    desafio usando funcao e estrutura if else

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("Hello World!")
+function calculator(vitorias, derrotas){
+    return vitorias - derrotas
+}
+calculator(100,30)
+let saldo = calculator(100,30)
+let rank = ""
+if(saldo < 10){
+    rank = "Ferro"
+}
+else if(saldo < 20){
+    rank = "Bronze"
+}
+else if(saldo < 50){
+    rank = "Prata"
+}
+else if(saldo < 80){
+    rank = "Ouro"
+}
+else if(saldo < 90){
+    rank = "Diamante"
+}
+else if(saldo < 100){
+    rank = "Lendário"
+}
+else {
+    rank = "Imortal"
+}
+console.log(`O Herói tem saldo de ${saldo} está no Rank ${rank}`)
